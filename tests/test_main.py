96187d6e9d70d4520fd7a1ef4fd9b9dd1f58455d
Taiga-Kawakami <taiga.kawakami@tcs.com>
2026-06-12 13:30:01 +0900

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,6 @@
 from src.main import add
+import pytest
 
-
-def test_add_integer_values():
-    assert add(2, 3) == 5
-
-
-def test_add_decimal_values():
-    assert add(1.2, 3.5) == 4
-
-
-def test_add_with_third_argument():
-    assert add(1.2, 3.5, 2.8) == 6
+def test_add():
+  assert add(2, 3) == 5
+  assert add(1.2, 3.5) == 4
